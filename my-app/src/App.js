979604364be9df255a74b985f8712fe4a84edd0a
bedenkog <bedenkog@coventry.ac.uk>
2018-10-26 12:41:00 +0100

    Finished header from lab example

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,18 +4,22 @@ import React, { Component } from 'react';
 import './App.css';
 //import the Header component
 import Header from './components/header/Header';
+import react_logo from './chef-hat.png'; 
 
 //define a new class for the App
 class App extends Component {
 
   render() {
-
     //this app will render the header component
     return (
       <div>
-      <Header/>
+      <Header title="Yummy Recipe Blog" logo={react_logo} onSearchClick={this.onSearch} backgroundColor="#7FFF2B"/>
       </div>
     );
+  }
+
+  onSearch(term){  
+    console.log("search on term:" + term);
   }
 
 }
--- my-app/src/components/header/Header.js
+++ my-app/src/components/header/Header.js
@@ -1,23 +1,45 @@
 import React, { Component } from 'react';
 import './Header.css';
-import react_logo from '../../logo.svg';
 
 class Header extends Component {
 
-    handleSearchSubmit(event){
+    constructor(props){
+        // Uses parent 'Component' properties variables
+        super(props);
+        
+        // Ensures that the functions understand what 'this' object is
+        this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
 
+        // Maintains the state of the header's attributes, including the value of the search field in the header
+        this.state = {
+            searchTerm : '',
+            headerStyle:{backgroundColor:this.props.backgroundColor} 
+        }
+    }
+
+    handleSearchSubmit(event){
         //prevent the form to be submitted to its action url
         event.preventDefault();
-        console.log("search button was clicked");
+
+        // Use the passed function from the parent
+        this.props.onSearchClick(this.state.searchTerm); 
+    }
+
+    handleInputChange(event){
+        // this.state.searchTerm = event.target.value
+
+        // Update the state of the searchTerm value to whatever was entered in the field
+        this.setState({searchTerm : event.target.value})
     }
 
     render() {
 
         return (
         //this is JSX code which is very similar to HTML we already know 
-        <div className="header">
-            <img src={react_logo} alt="React logo" /><a href="#default" classNa
-            me="logo"> React App Header</a>
+        <div className="header" style={this.state.headerStyle}>
+            <img src={this.props.logo} alt="React logo" />
+            <a href="#default" className="logo"> {this.props.title}</a>
             <div className="header-right">
                 <div className="search-container">
                     <form action="">
