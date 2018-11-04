0b76539a6a695ee3e74adc1c061062acbdce04f4
Genaro Bedenko (bedenkog) <bedenkog@coventry.ac.uk>
2018-11-04 11:29:39 +0000

    Initial react components for home page dashboard

---
+++ my-app/src/components/homePage/highestRatedRecipes/HighestRatedRecipes.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class HighestRatedRecipes extends Component {
+  
+    constructor(props){
+        super(props);
+
+        this.state = {
+        };
+        
+    }
+
+    render() {
+
+        return (
+
+            <div className="HighestRatedRecipes">
+                <h3>Highest Rated Recipes:</h3>
+                <ol>
+                    <li>...</li>
+                    <li>...</li>
+                    <li>...</li>
+                    <li>...</li>
+                    <li>...</li>
+                </ol>
+            </div>
+        );
+    }
+}
+export default HighestRatedRecipes;
---
+++ my-app/src/components/homePage/mostViewedRecipes/MostViewedRecipes.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class MostViewedRecipes extends Component {
+  
+    constructor(props){
+        super(props);
+
+        this.state = {
+        };
+        
+    }
+
+    render() {
+
+        return (
+
+            <div className="MostViewedRecipes">
+                <h3>Most Viewed Recipes:</h3>
+                <ol>
+                    <li>...</li>
+                    <li>...</li>
+                    <li>...</li>
+                    <li>...</li>
+                    <li>...</li>
+                </ol>
+            </div>
+        );
+    }
+}
+export default MostViewedRecipes;
